96c68519eb2ba8035c4fe9e72f4540b247001d20
Blackout1471 <Emil.rauff.andersen@hotmail.com>
2022-08-19 18:20:29 +0200

    Merged socketserver and main

--- Source/Server/SocketServer.h
+++ Source/Server/SocketServer.h
@@ -2,26 +2,29 @@
 #include "IServer.h"
 namespace MQTT {
 	namespace Server {
-		class SocketServer : IServer
+		class SocketServer : public IServer
 		{
 		public:
-			SocketServer(int port) : m_Port(port) {};
-			~SocketServer() {};
+			SocketServer(int port);
+			~SocketServer();
 			// Inherited via IServer
 			virtual void Start() override;
 			virtual void Stop() override;
 			virtual void Disconnect(const Client& client) override;
 			virtual void Send(const Client& client, const std::vector<unsigned char>& data) override;
+
 		private:
 			virtual void ConfigureAddressInfo(int port);
 			virtual void CreateSocket();
 			virtual void SetupTCP();
 			virtual void Listen();
 			virtual void Accept();
+
+			static void ReadClientData(const Client& client, const SocketServer& server);
 		private:
 			int m_Port;
+			int m_Socket;
 			std::vector<Client> m_Clients;
 		};
 	}
-}
-
+};
