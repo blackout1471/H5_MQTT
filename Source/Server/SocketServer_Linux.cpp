7f236442f4aca0819942c503b9f15ee2cfb4ff20
EADR (Emil Andersen) <EADR@nnit.com>
2022-08-19 12:18:57 +0200

    Fixed linux & windows

---
+++ Source/Server/SocketServer_Linux.cpp
@@ -0,0 +1,20 @@
+#include "mqttpch.h"
+#include "SocketServer.h"
+
+namespace MQTT {
+	namespace Server {
+		void SocketServer::Start()
+		{
+
+		}
+		void SocketServer::Stop()
+		{
+		}
+		void SocketServer::Disconnect(const Client& client)
+		{
+		}
+		void SocketServer::SendMessage(const Client& client, const std::vector<unsigned char>& data)
+		{
+		}
+	}
+}
