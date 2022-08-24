68dc95c0b51206b1092c739c0c80512678801faa
Peter Cholub <pch@logisnap.com>
2022-08-24 12:05:59 +0200

    Added try catch

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,9 +1,13 @@
 #include "mqttpch.h"
 #include "Server/MqttService.h"
-
+#include <iostream>
 int main() {
+	try {
 
-	MQTT::Server::MqttService mqttService = MQTT::Server::MqttService();
-
+		MQTT::Server::MqttService mqttService = MQTT::Server::MqttService();
+	}
+	catch (const std::exception& e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
 	return 0;
 }
