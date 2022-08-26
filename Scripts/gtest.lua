eec2ddab8b16b43e1786e88775b3257221f41714
Ripiter <44769203+Ripiter@users.noreply.github.com>
2022-08-26 09:44:58 +0200

    Peter/gtest setup (#8)
    * Added gtest
    
    * Added cpp version
    
    * Updated files path in lua
    
    * updated gtest
    
    * Added base unit test
    
    Co-authored-by: Peter Cholub <pch@logisnap.com>

---
+++ Scripts/gtest.lua
@@ -0,0 +1,39 @@
+project "gtest"
+	location "../vendor/googletest/googletest"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+    cppdialect "C++20"
+
+	targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+
+    files
+    {
+        "../vendor/googletest/googletest/**.h",
+        "../vendor/googletest/googletest/**.hpp",
+        "../vendor/googletest/googletest/src/gtest-all.cc",
+    }
+
+    includedirs
+    {
+		"../vendor/googletest/googletest/include",
+		"../vendor/googletest/googletest/"
+    }
+
+    filter "configurations:Debug"
+        defines "_DEBUG"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines "_RELEASE"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        defines "_DIST"
+        runtime "Release"
+        optimize "on"
