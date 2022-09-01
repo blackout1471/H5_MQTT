9a3c6ece37ddf5ccb707f33e0a328b778d502365
Peter Cholub <pch@logisnap.com>
2022-09-01 11:17:23 +0200

    Added moved logic to the subscriber

--- Source/Protocol/Managers/SubscribeManager.cpp
+++ Source/Protocol/Managers/SubscribeManager.cpp
@@ -1,6 +1,6 @@
 #include "mqttpch.h"
 #include "SubscribeManager.h"
-
+#include "Protocol/Converters/SubscribeAcknowledgementConverter.h"
 
 namespace MQTT {
 	namespace Protocol {
@@ -17,6 +17,55 @@ namespace MQTT {
 
 			return nullptr;
 		}*/
+
+		void SubscribeManager::AddToSubscriptions(std::string clientID, SubscribePackage subscribePackage)
+		{
+			for (auto subscribeTopic : subscribePackage.SubscribePayload.Topics)
+			{
+				auto subClient = new Protocol::SubscribeClient(clientID, subscribeTopic.QoS);
+				auto parentWildcard = subscribeTopic.HaveChild ? Protocol::NoWildcard : subscribeTopic.Wildcard;
+
+				Protocol::BTree* matchingTree = GetParentBTree(subscribeTopic.Paths[0], parentWildcard);
+				Protocol::BTree* parentTree = nullptr;
+
+				for (int i = 0; i < subscribeTopic.Paths.size(); i++)
+				{
+					// Check if there is any full match of the current topic
+					if (parentTree != nullptr)
+						matchingTree = parentTree->GetFullMatch(subscribeTopic.Paths.at(i), subscribeTopic.Wildcard);
+
+					if (matchingTree != nullptr) {
+						parentTree = matchingTree;
+						continue;
+					}
+
+					if (parentTree == nullptr)
+					{
+						// its the last topic, set the client 
+						if ((i + 1) >= subscribeTopic.Paths.size())
+							parentTree = Protocol::BTree::NewBTree(subClient, subscribeTopic.Paths.at(i), subscribeTopic.Wildcard);
+
+						else
+							parentTree = Protocol::BTree::NewBTree(subscribeTopic.Paths.at(i), Protocol::NoWildcard);
+
+						AddParentTree(parentTree);
+					}
+					else
+					{
+						// its the last topic
+						if ((i + 1) >= subscribeTopic.Paths.size())
+							parentTree = Protocol::BTree::NewBTree(parentTree, subClient, subscribeTopic.Paths.at(i), subscribeTopic.Wildcard);
+						else
+							parentTree = Protocol::BTree::NewBTree(parentTree, subscribeTopic.Paths.at(i));
+					}
+				}
+			}
+		}
+
+	
+
+
+
 
 		BTree* SubscribeManager::GetParentBTree(std::vector<unsigned char> topic, SubscribeTopicWildcardType wildcard)
 		{
@@ -43,6 +92,25 @@ namespace MQTT {
 				delete m_Subscriptions[i];
 			}
 		}
+
+		std::vector<unsigned char> SubscribeManager::CreateSubAckBuffer(const Protocol::SubscribePackage& subPackage)
+		{
+			Protocol::Converters::SubscribeAcknowledgementConverter converter;
+
+			Protocol::SubscribeAcknowledgementPackage ackPackage = Protocol::SubscribeAcknowledgementPackage();
+			ackPackage.Header.PackageType = Protocol::ControlPackageType::SubAck;
+			ackPackage.VariableHeader.PacketIdentifier = subPackage.SubscribeVariableHeader.PacketIdentifier;
+
+
+			for (int i = 0; i < subPackage.SubscribePayload.Topics.size(); i++)
+			{
+				auto q = (SubscribeAcknowledgementQoS)subPackage.SubscribePayload.Topics.at(i).QoS;
+				ackPackage.Payload.payload.push_back(q);
+			}
+
+			return converter.ToBuffer(ackPackage);
+		}
+
 
 	}
 }
--- Source/Protocol/Managers/SubscribeManager.h
+++ Source/Protocol/Managers/SubscribeManager.h
@@ -6,8 +6,12 @@ namespace MQTT {
 		{
 			std::vector<BTree*> m_Subscriptions;
 		public:
+
+
 			// Searches only in the parents
 			BTree* GetParentBTree(std::vector<unsigned char> topic, SubscribeTopicWildcardType wildcard);
+			void AddToSubscriptions(std::string clientID, SubscribePackage subscribePackage);
+			std::vector<unsigned char> CreateSubAckBuffer(const Protocol::SubscribePackage& subPackage);
 			void AddParentTree(BTree* bTree);
 			void DeleteTrees();
 		};
