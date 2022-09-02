b7464daddb742075d71e42f529d236baa1c3709b
Peter Cholub <pch@logisnap.com>
2022-09-02 09:17:19 +0200

    Added comments and moved the private variables

--- Source/Protocol/Managers/SubscribeManager.h
+++ Source/Protocol/Managers/SubscribeManager.h
@@ -9,20 +9,24 @@ namespace MQTT {
 	namespace Protocol {
 		class SubscribeManager
 		{
-			std::vector<BTree*> m_Subscriptions;
-			void AddSubscribers(std::vector<SubscribeClient>& to, std::vector<SubscribeClient*>& from);
-
 		public:
-
 			bool ValidPackage(const SubscribePackage& subscribePackage);
-
 			// Searches only in the parents
 			BTree* GetParentBTree(std::vector<unsigned char> topic, SubscribeTopicWildcardType wildcard);
+			// Adds client to subscriptions if the subscribe package is valid
 			void AddToSubscriptions(std::string clientID, SubscribePackage subscribePackage);
 			std::vector<unsigned char> CreateSubAckBuffer(const Protocol::SubscribePackage& subPackage);
+
+			// Finds all clients that are subscribed to the given topic, or are subscriber of a parent with wildcard
 			std::vector<SubscribeClient> GetSubscribedClients(std::vector<unsigned char> topic);
 			void AddParentTree(BTree* bTree);
 			void DeleteTrees();
+
+		private:
+			std::vector<BTree*> m_Subscriptions;
+
+			// Copies a list of subscibers from one list to another, if client already exists in 'to' the QoS of that client is updated
+			void AddSubscribers(std::vector<SubscribeClient>& to, std::vector<SubscribeClient*>& from);
 		};
 
 	}
