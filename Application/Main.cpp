9380e250dcd46743c66717aed407f4b9265aa7ac
Peter Cholub <pch@logisnap.com>
2022-09-05 18:24:04 +0200

    Added port param support

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -6,10 +6,21 @@
 
 using namespace MQTT::Server;
 
+const int SOCKET_PORT = 1883;
 
-int main() {
-	try {
-		auto socketServer = SocketServer(1883);
+int getPort(int argc, char** argv)
+{
+	for (int i = 0; i < argc; ++i)
+		if (strcmp(argv[i], "-p") == 0)
+			return atoi(argv[i + 1]);
+
+	return SOCKET_PORT;
+}
+
+int main(int argc, char** argv) {
+	try
+	{
+		auto socketServer = SocketServer(getPort(argc, argv));
 		MqttService mqttService = MqttService(&socketServer);
 		mqttService.Start();
 	}
